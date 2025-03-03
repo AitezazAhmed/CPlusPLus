edf4c7880f650a4a60e0ae215a1e0862de390da5
AitezazAhmed <stoneghold4103@gmail.com>
2025-03-03 17:36:32 +0500

    .

---
+++ 07_exercise/exercise.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+// show total sales sale tax and countytax and total tax on the console
+int main(){
+
+    double totalSales=95000;
+    cout<<"TotalSales $"<<totalSales <<endl;;
+
+    double saleTaxRate=0.04;
+    double saleTax=totalSales * saleTaxRate;
+    cout<<"SaleTax $"<< saleTax<< endl;
+
+    double countyTaxRate=0.02;
+    double countyTax=totalSales * countyTaxRate;
+    cout<<"CountyTax $"<< countyTax<< endl;
+
+    double totalTax=saleTax+countyTax;
+    cout<<"TotalTax $"<<totalTax << endl;
+
+}
---
+++ 08_working with input/workingwithinput.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//     double x,y;
+//     cout<<"Enter the value of x:" <<endl;
+//     cin>>x;
+//     cout<<"Enter the value of y:" <<endl;
+//     cin>>y;
+//  cout<<x+y<<endl;
+// }
+
+int main() {
+    double fahrenheit;
+    cout << "Enter the temperature in Fahrenheit: ";
+    cin >> fahrenheit;
+
+    double celsius = (fahrenheit - 32) * (5.0 / 9.0);  // Use 5.0 / 9.0 to ensure floating-point division
+    cout << "Temperature in Celsius: " << celsius << endl;
+}
