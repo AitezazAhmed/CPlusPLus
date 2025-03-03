8be8f2fdde47144321a85a0e27e465ee09cf7182
AitezazAhmed <stoneghold4103@gmail.com>
2025-03-04 01:39:23 +0500

    initializingthevalue.cpp

---
+++ 11_initializing the value/initializingthevalue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+   double price=99.99;
+   float intrestRate= 3.67F; // use at the end F when we use float
+   long fileSize =99000L; // use at the end L when we use long
+   char name='a'; // use string when we use char
+   bool isvalid=true; // use true or false when use bool
+}
+// int main(){
+//    int number=0;// initializing to zero the number
+//    int number ={}; // this is another way o intializing the value to zero
+//    cout<<number << endl;
+// }
