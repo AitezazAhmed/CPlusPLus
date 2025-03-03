a72c8e0b21e9362efe68fd837c72bd12298bdb3d
AitezazAhmed <stoneghold4103@gmail.com>
2025-03-04 01:20:04 +0500

    standardlibrary.cpp

---
+++ 09_workingwithstandardlibrary/standardlibrary.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath> // there are so many so we can check on google by cmath refrence
+using namespace std;
+int main(){
+ double value= ceil(1.2); // round up function Answer is 2
+ cout<<value<<endl;
+}
+
+// int main(){
+//     double value= floor(1.2); // round down function Answer is 1
+//     cout<<value<<endl;
+//    }
+
+// int main(){
+//     double value= pow(2,3); // 2 with power of 3 so the answer is 8
+//     cout<<value<<endl;
+//    }
+
