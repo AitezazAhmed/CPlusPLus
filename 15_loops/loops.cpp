a9f57625f1ae6590ab17cdf3fe7667708649ea8c
AitezazAhmed <stoneghold4103@gmail.com>
2025-03-05 09:17:47 +0500

    loops.cpp

---
+++ 15_loops/loops.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+int index=0;
+while (index<100)
+{
+    cout<<"Index is "<<index<<endl;
+    index=index+1;
+} // while loop
+
+
+// do
+// {
+//     cout<<"Index is "<<index<<endl;
+// } while (index>32);
+// index=index+1;     //  must run once either condition is false
+
+
+
+//  for (int i = 0; i<100; i++)
+// {
+//     cout<<"Index is "<<i<<endl;
+// }
+}
+
