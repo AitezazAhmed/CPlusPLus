1c9599a2f9153b371347ab58c888cc5ee3da697c
AitezazAhmed <stoneghold4103@gmail.com>
2025-03-09 16:37:04 +0500

    string.cpp

---
+++ 19_strings/string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>   // by this we use functions of the string
+using namespace std;
+main(){
+   string name="Aitezaz";
+   cout<<name<<endl;
+   cout<<name.length()<<endl;
+   cout<<name.append("Ahmed")<<endl;
+   cout<<name.substr(0,3)<<endl;
+}
