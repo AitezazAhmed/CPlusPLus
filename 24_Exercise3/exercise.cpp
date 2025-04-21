4dbe9746c8412a4bb590aec4e941fb7f5ebcedfd
AitezazAhmed <stoneghold4103@gmail.com>
2025-04-22 00:31:30 +0500

    exercise.cpp

---
+++ 24_Exercise3/exercise.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double radius,area;
+    cout<<"Enter the radius of the circle: ";
+    cin>>radius;
+    area= 3.14 *radius*radius;
+    cout<<"Area="<<area;
+
+}
