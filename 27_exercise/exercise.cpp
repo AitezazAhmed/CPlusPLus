c527f66745ebd4837ea923538fae5967b182779a
AitezazAhmed <stoneghold4103@gmail.com>
2025-05-05 23:18:53 +0500

    exercise.cpp

---
+++ 27_exercise/exercise.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+// Function to find sum
+int findSum(int a, int b, int c) {
+    return a + b + c;
+}
+
+// Function to find subtraction (a - b - c)
+int findSubtract(int a, int b, int c) {
+    return a - b - c;
+}
+
+// Function to find max
+int findMax(int a, int b, int c) {
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max;
+}
+
+// Function to find min
+int findMin(int a, int b, int c) {
+    int min = a;
+    if (b < min) min = b;
+    if (c < min) min = c;
+    return min;
+}
+
+int main() {
+    int a, b, c;
+    cout << "Enter value of a: ";
+    cin >> a ;
+    cout << "Enter value of b: ";
+    cin >> b ;
+    cout << "Enter value of c: ";
+    cin >> c;
+    int choice;
+    cout << "\nChoose an operation:\n";
+    cout << "1. Sum\n";
+    cout << "2. Subtract\n";
+    cout << "3. Max\n";
+    cout << "4. Min\n";
+    cout << "Enter your choice (1-4): ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            cout << "Sum = " << findSum(a, b, c) << endl;
+            break;
+        case 2:
+            cout << "Subtraction = " << findSubtract(a, b, c) << endl;
+            break;
+        case 3:
+            cout << "Max = " << findMax(a, b, c) << endl;
+            break;
+        case 4:
+            cout << "Min = " << findMin(a, b, c) << endl;
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+    }
+
+    return 0;
+}
